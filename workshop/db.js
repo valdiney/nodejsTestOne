148c85a2dc95653183cce5586bd9cafb2350effa
Valdiney <valdiney.2@hotmail.com>
2019-06-14 11:31:42 -0300

    test

--- workshop/db.js
+++ workshop/db.js
@@ -1,6 +1,7 @@
 var mongoClient = require('mongodb').MongoClient;
-mongoClient.connect('mongodb://valdiney:33473347@cluster0-shard-00-00-i0akk.mongodb.net:27017,cluster0-shard-00-01-i0akk.mongodb.net:27017,cluster0-shard-00-02-i0akk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+//mongoClient.connect('mongodb://valdiney:33473347@cluster0-shard-00-00-i0akk.mongodb.net:27017,cluster0-shard-00-01-i0akk.mongodb.net:27017,cluster0-shard-00-02-i0akk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
 //mongoClient.connect('mongodb://localhost:27017/meubanco')
+mongoClient.connect('mongodb+srv://valdiney:33473347@cluster0-i0akk.mongodb.net/test?retryWrites=true&w=majority')
  .then(conn => global.conn = conn)
  .catch(err => console.log(err))
 
