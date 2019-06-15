dc998edf6696ca7e71403719c15705a5ac847776
Valdiney <valdiney.2@hotmail.com>
2019-06-15 16:06:41 -0300

    test

--- workshop/routes/index.js
+++ workshop/routes/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://valdiney:33473347@cluster0-shard-00-00-i0akk.mongodb.net:27017,cluster0-shard-00-01-i0akk.mongodb.net:27017,cluster0-shard-00-02-i0akk.mongodb.net:27017/meubanco?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+
 router.get('/', function(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		
+		var customers = db.collection('customers').find({}).sort({nome: 1}).toArray(function(err, result) {
+             console.log(result);
+             var docs = result;
+
+             res.render('index',  {docs})
+
+		})
+		
+
+	})
+})
+
+
+/* GET home page. */
+/*router.get('/', function(req, res) {
   global.db.findAll((e, docs) => {
   	if (e) {
   		return console.log(e);
@@ -15,7 +36,7 @@ router.get('/', function(req, res) {
   })
   
 });
-
+*/
 
 /* GET tela cadastrar. */
 router.get('/cadastrar', function(req, res) {
@@ -41,8 +62,11 @@ router.post('/editar', function(req, res) {
   const nome = req.body.nome;
   const idade = req.body.idade;
   const sexo = req.body.sexo;
+  const profissao = req.body.profissao;
+
+  console.log(profissao);
   
-  global.db.updateOne(id, {nome, idade, sexo}, (e, docs) => {
+  global.db.updateOne(id, {nome, idade, sexo, profissao}, (e, docs) => {
   	if (e) {
   		return console.log(e);
   	}
